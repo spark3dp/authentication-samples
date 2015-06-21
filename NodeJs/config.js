d4e52cbb609a95ff988592e5c6a46fe7314154cd
Greg Rashkevitch <rashkevitch@gmail.com>
2015-06-21 16:32:21 +0300

    Reorder code + calculate callback_uri inside the code instead of config

--- NodeJs/config.js
+++ NodeJs/config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	APP_KEY: '',
 	APP_SECRET: '',
-	REDIRECT_URI: 'http://localhost:3000/callback', // http://example.com/callback
 	ENV: 'sandbox' //sandbox or production
 };
--- NodeJs/server.js
+++ NodeJs/server.js
@@ -2,10 +2,25 @@ var config = require('./config.js');
 
 var APP_KEY = config.APP_KEY,
     APP_SECRET = config.APP_SECRET,
-    REDIRECT_URI = config.REDIRECT_URI,
     API_URL = (console.ENV === 'production')  ? 'https://api.spark.autodesk.com/api/v1' 
                                               : 'https://sandbox.spark.autodesk.com/api/v1';
 
+
+var express = require('express'),
+    request = require('request');
+    app = express();
+
+app.set('views', './views')
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+var accessToken = null,
+    guestToken = null,
+    redirectUri;
+
+
+/*** FUNCTIONS ***/
 
 /**
  * Encode string in base64
@@ -17,43 +32,22 @@ var toBase64 = function(str){
 }
 
 
-var express = require('express'),
-    request = require('request');
-    app = express();
-
-app.set('views', './views')
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-var accessToken = null;
-var guestToken = null;
-
-var authorization_uri = API_URL + 
-                '/oauth/authorize' +
+/**
+ * Spark Authorization URL endpoint 
+ */
+var authorizationUrl = function(req){
+  var fullUrl = req.protocol + '://' + req.get('host');
+  redirectUri = fullUrl + '/callback';
+  return API_URL + '/oauth/authorize' +
                 '?response_type=code' +
                 '&client_id=' + APP_KEY + 
-                '&redirect_uri=' + REDIRECT_URI;
+                '&redirect_uri=' + redirectUri;
+}
 
-
-// Show the index page
-app.get('/', function (req, res) {
-  res.render('index.html',{accessToken:accessToken,guestToken:guestToken});
-});
-
-// Initial page redirecting to Spark Login
-app.get('/auth', function (req, res) {
-    res.redirect(authorization_uri);
-});
-
-// Callback service parsing the authorization token and asking for the access token
-// See API reference - https://spark.autodesk.com/developers/reference/authentication?deeplink=%2Freference%2Foauth-2.0%2Faccess-token
-app.get('/callback', function (req, res) {
-
-  var code = req.query.code,
-      url = API_URL + '/oauth/accesstoken',
-      params = "code=" + code + "&grant_type=authorization_code&response_type=code&redirect_uri=" + REDIRECT_URI;
-
-
+/**
+ * General POST request to /oauth/accesstoken - used for getting access and guest tokens
+ */
+var doRequest = function(url, params,callback){
   var contentLength = params.length,
     headers = {
       'Authorization': 'Basic ' + toBase64(APP_KEY + ':' + APP_SECRET),
@@ -68,41 +62,56 @@ app.get('/callback', function (req, res) {
     body: params,
     method: 'POST'
   }, function (err, result, body) {
+    
+    var resp = JSON.parse(body);
+    callback(resp);
+  });
+}
 
-    accessToken = JSON.parse(body);
-    console.log(accessToken);
+
+
+/*** ROUTES ***/
+
+// Show the index page
+app.get('/', function (req, res) {
+  res.render('index.html',{accessToken:accessToken,guestToken:guestToken});
+});
+
+// Initial page redirecting to Spark Login
+app.get('/auth', function (req, res) {
+    res.redirect(authorizationUrl(req));
+});
+
+// Callback service parsing the authorization token and asking for the access token
+// See API reference - https://spak.autodesk.com/developers/reference/authentication?deeplink=%2Freference%2Foauth-2.0%2Faccess-token
+app.get('/callback', function (req, res) {
+  var code = req.query.code,
+      url =  API_URL + '/oauth/accesstoken',
+      params = "code=" + code + "&grant_type=authorization_code&response_type=code&redirect_uri=" + redirectUri;
+
+  doRequest(url, params, function(token){
+    if (token.access_token){
+      accessToken = token;
+    }else{
+      console.log('error getting token', token);
+    }
     res.redirect('/');
   });
-
 });
 
 // Guest token service
 // See API reference - https://spark.autodesk.com/developers/reference/authentication?deeplink=%2Freference%2Foauth-2.0%2Fguest-token
 app.get('/guest_token', function(req, res){
-  var url = API_URL + '/oauth/accesstoken',
-    params = "grant_type=client_credentials",
-    contentLength = params.length,
-    headers = {
-      'Authorization': 'Basic ' + toBase64(APP_KEY + ':' + APP_SECRET),
-      'Content-Type' : 'application/x-www-form-urlencoded',
-      'Content-Length': contentLength
-    };
-
-  //call the accesstoken endpoint
-  request({
-    headers: headers,
-    uri: url,
-    body: params,
-    method: 'POST'
-  }, function (err, result, body) {
-
-    //return the guest token object (json)
-    guestToken = JSON.parse(body);
-    console.log(guestToken);
+  var params = "grant_type=client_credentials",
+      url =  API_URL + '/oauth/accesstoken';
+  doRequest(url, params, function(token){
+    if (token.access_token){
+      guestToken = token;
+    }else{
+      console.log('error getting token', token);
+    }
     res.redirect('/');
   });
-
-
 });
 
 // Refresh token service
@@ -110,26 +119,14 @@ app.get('/guest_token', function(req, res){
 app.get('/refresh_token', function(req, res) {
 
   if (accessToken){  
-    var url = API_URL + '/oauth/refreshtoken',
-      params = "grant_type=refresh_token&refresh_token=" + accessToken.refresh_token,
-      contentLength = params.length,
-      headers = {
-        'Authorization': 'Basic ' + toBase64(APP_KEY + ':' + APP_SECRET),
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Content-Length': contentLength
-      };
-
-    //call the refreshtoken endpoint
-    request({
-      headers: headers,
-      uri: url,
-      body: params,
-      method: 'POST'
-    }, function (err, result, body) {
-      //return the access token object (json)
-
-      accessToken = JSON.parse(body);
-      console.log(accessToken);
+    var params = "grant_type=refresh_token&refresh_token=" + accessToken.refresh_token,
+        url =  API_URL + '/oauth/refreshtoken';
+    doRequest(url, params, function(token){
+      if (token.access_token){
+        accessToken = token;
+      }else{
+        console.log('error getting token', token);
+      }
       res.redirect('/');
     });
   }else{
@@ -141,7 +138,6 @@ app.get('/refresh_token', function(req, res) {
 //logout endpoint
 app.get('/logout', function(req, res){
   accessToken = null;
-  guestToken = null;
   res.redirect('/');
 });
 
