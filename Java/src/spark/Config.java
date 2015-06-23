1daca462f55fcca17e74205bbc994833af42d654
Michael Rozman <michael.rozman@autodesk.com>
2015-06-23 14:55:13 +0300

    java UI

--- Java/src/spark/Config.java
+++ Java/src/spark/Config.java
@@ -5,7 +5,7 @@ package spark;
  */
 public class Config {
 
-    //TODO: in your app settings(in Developer Portal) you should change your "Callback URL" field to "http://localhost:8089/callback"
+    //TODO: in your app settings(in Developer Portal) you should change your "Callback URL" field to "http://localhost:8089/"
 
     //TODO: enter your CLIENT_ID from Developer Portal App page
     public static String CLIENT_ID="YOUR_APP_KEY";
@@ -16,5 +16,6 @@ public class Config {
     public static String SPARK_ENDPOINT = "https://sandbox.spark.autodesk.com";
     public static String LOGIN_URI= SPARK_ENDPOINT + "/api/v1/oauth/authorize";
     public static String TOKEN_URI= SPARK_ENDPOINT + "/api/v1/oauth/accesstoken";
+    public static String REFRESH_TOKEN_URI= SPARK_ENDPOINT + "/api/v1/oauth/refreshtoken";
 
 }
