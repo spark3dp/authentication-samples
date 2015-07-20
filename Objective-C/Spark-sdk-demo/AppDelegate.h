2b14e9fb0deeb2476164af81eae48859b63bed94
Roi Yarden <roi@cmycasa.com>
2015-07-20 14:31:15 +0300

    Objective-C sample

---
+++ Objective-C/Spark-sdk-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spark-sdk-demo
+//
+//  Created by Autodesk on 6/25/15.
+//  Copyright (c) 2015 Autodesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
